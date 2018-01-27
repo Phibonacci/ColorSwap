c807c23e6a0ee33c2a913048ed08bbb96bdf364e
Jean Fauquenot <jean.fauquenot@gmail.com>
2018-01-27 21:40:35 +0100

    Add physics to Player

--- player.js
+++ player.js
@@ -2,24 +2,94 @@ class Player {
 	constructor (playerId) {
 		if (playerId == 1)
 		{
-			this.sprite = window.game.add.sprite(64, 64, 'platformer_complete', 'alienBlue_walk1.png');
+			this.sprite = game.add.sprite(64, 64, 'platformer_complete', 'alienBlue_walk1.png');
 		}
 		else if (playerId == 2)
 		{
-			this.sprite = window.game.add.sprite(64, 64, 'platformer_complete', 'alienYellow_walk1.png');
+			this.sprite = game.add.sprite(64, 64, 'platformer_complete', 'alienYellow_walk1.png');
+		}
+		this.initializeBody();
+		this.initializeAnimations();
+	}
+	
+	initializeAnimations () {
+		this.sprite.animations.add('walk', [31, 32], 5, true);
+	}
+	
+	initializeBody () {
+		game.physics.arcade.enable(this.sprite);
+		this.sprite.body.bounce.y = 0.2;
+		this.sprite.body.gravity.y = 300;
+		this.sprite.body.collideWorldBounds = true;
+	}
+	
+	update (delta) {
+		if (this.rightKey()) {
+			this.moveRight(delta);
+		}
+		else if (this.leftKey()) {
+			this.moveLeft(delta);
+		}
+		else {
+			this.sprite.body.velocity.x = 0;
+		}
+		if (this.jumpKey()) {
+			this.jump(delta);
+		}
+	}
+
+	reverseSprite(direction) {
+		this.sprite.anchor.setTo(.5,.5);
+		if (direction * this.sprite.scale.x < 0) {
+			this.sprite.scale.x *= -1;
 		}
 	}
 	
-	update(delta)
-	{
-		console.log(delta);
-		var movement = delta * 100;
-		this.sprite.x += movement;
+	rightKey () {
+		return game.input.keyboard.isDown(Phaser.KeyCode.D) || game.input.keyboard.isDown(Phaser.KeyCode.RIGHT);
 	}
+
+	leftKey () {
+		return game.input.keyboard.isDown(Phaser.KeyCode.Q) || game.input.keyboard.isDown(Phaser.KeyCode.LEFT);
+	}
+	
+	jumpKey () {
+		return game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR) || game.input.keyboard.isDown(Phaser.KeyCode.UP);
+	}
+	
+	movement (delta) {
+		return delta * 30000;
+	}
+	
+	moveRight (delta) {
+		if (this.sprite.animations.currentAnime != 'walk') {
+			this.sprite.animations.play('walk');
+		}
+		if (this.sprite.scale.x < 0) {
+			this.reverseSprite(1);
+		}
+		this.sprite.body.velocity.x = this.movement(delta);
+	}
+
+	moveLeft (delta) {
+		if (this.sprite.animations.currentAnime != 'walk') {
+			this.sprite.animations.play('walk');
+		}
+		if (this.sprite.scale.x > 0) {
+			this.reverseSprite(-1);
+		}
+		this.sprite.body.velocity.x = -this.movement(delta);
+	}
+	
+	jump (delta) {
+		if (this.sprite.body.touching.down && hitPlatform) {
+			this.sprite.body.velocity.y = this.movement(delta);
+		}
+	}
+
 	
 	animate () {
 		// frames = new FrameData();
 		// frames.Add(new Frame(0,  "alienGreen_walk1.png");
-		// this.sprite.animation.add('walk', Phaser.Animation.generateFrameNames('platformer_complete', ))
 	}
 }
